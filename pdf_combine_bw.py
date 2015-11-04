e221fdd485ab521128a1488a39bc5085d7782e01
Daniel Mulholland <dan.mulholland@gmail.com>
2015-11-04 17:24:41 +1300

    Modified to enable right-click context menu operation from Windows Explorer
    Modified to enable right-click context menu operation from Windows
    Explorer
    * uses sys.argv[1]
    * the .reg file relies on installation in to
           C:\PA_Applications\pdf-grid-drawings\pdf_combine_bw.py
    * bumped version to 0.5

--- pdf_combine_bw.py
+++ pdf_combine_bw.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python2.7
 #
-# Walks through a folder and all subfolders:
-#  - Finds pdfs
+# Takes a ZIP file from a right-click menu in explorer or a command line argument:
 #  - Combines pdfs into a single file
 #  - Scales all PDFs to the same size
 #  - Creates one bookmark per file, provides zoom level for whole pdf
@@ -12,12 +11,13 @@
 # TODO:
 # - OCR the output (perhaps with tesseract-ocr-3.02-win32-portable.zip, http://documentup.com/virantha/pypdfocr/)
 # - presently abandoned as too difficult/time consuming.
+# - bring back previous functionality
 
 __author__ = "Daniel Mulholland"
 __copyright__ = "Copyright 2015, Daniel Mulholland"
 __credits__ = ["Whoever wrote ghostscript, python!"]
 __license__ = "GPL"
-__version__ = "0.4"
+__version__ = "0.5"
 __maintainer__ = "Daniel Mulholland"
 __email__ = "dan.mulholland@gmail.com"
 
@@ -29,9 +29,9 @@ import re
 import zipfile
 import glob
 import time
+import tempfile
+import datetime
 
-INPUT_FOLDER = "in"
-BASE_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)),INPUT_FOLDER)
 GS_BINARY_PATH = r'C:\Program Files\gs\gs9.15\bin\gswin64c.exe'
 PDF_EXTENSION = '.pdf'
 KEEP_FILES = ['combined.pdf', 'pdf_combine_bw.py', 'pdfmerge.py', 'pdf_combine_bw.pyc', 'pdfmerge.pyc']
@@ -48,16 +48,16 @@ def unzip(source_filename, dest_dir):
                 extracteditems.append(member.filename)
     return extracteditems
     
-def real_start(zip_file_name):
+def real_start(temporary_folder, zip_file_name):
     # create a list / array
     file_list = []
     list_to_compile = []
 
     # walk through all folders in script folder
-    for folder, subs, files in os.walk(BASE_PATH):
+    for folder, subs, files in os.walk(temporary_folder):
         for filename in files:
             # only look at pdfs based on the extension
-            if filename[-3:] == 'pdf' and filename not in KEEP_FILES:
+            if filename[-3:] == 'pdf':
                 file_list.append([folder, filename])
     
     sortable = []
@@ -70,37 +70,42 @@ def real_start(zip_file_name):
     # this ensures that peculiarity in drawing file name case does not impact ordering
     sortable.sort(key=lambda row: (row[1].upper() + re.sub("[^0-9]", "", row[2]).zfill(4)), reverse=False)
 
-    chosen_name = os.path.join(BASE_PATH,zip_file_name[0:-4]+PDF_EXTENSION)
-    
-    merge_and_create_bookmarks(sortable, chosen_name)
+    final_location_and_name = zip_file_name[0:-4]+PDF_EXTENSION
+    chosen_name = os.path.join(temporary_folder,os.path.basename(zip_file_name)[0:-4]+PDF_EXTENSION)
+
+    merge_and_create_bookmarks(sortable, chosen_name, temporary_folder)
 
     # create ps file to allow processing of colour directives
     output_name_ps = "\"" + chosen_name[0:-3]+ 'ps'  + "\""
     ps_output_options = "-dNOPAUSE -dBATCH -sDEVICE=ps2write"    
     output_command = " ".join([GS_BINARY_PATH, ps_output_options, "-sOutputFile=" + output_name_ps, "\"" + chosen_name +  "\""])
-    p = sub.Popen(output_command, stdout=sub.PIPE, stderr=sub.PIPE, cwd=BASE_PATH)
+
+    p = sub.Popen(output_command, stdout=sub.PIPE, stderr=sub.PIPE, cwd=temporary_folder)
     output, errors = p.communicate()
     #print output, errors
     if errors != "": print errors
     
     # convert file to pdf and process colour
     # http://superuser.com/questions/200378/converting-a-pdf-to-black-white-with-ghostscript
-    ps_output_options = " ".join(["-o " + "\"" +  chosen_name + "\"", "-sDEVICE=pdfwrite", "\"" + os.path.join(BASE_PATH,TOC_FILE) +  "\"", "-c \"/setrgbcolor{0 mul 3 1 roll 0 mul 3 1 roll 0 mul 3 1 roll 0 mul add add setgray} def\"", "-f " + output_name_ps])    
+
+    ps_output_options = " ".join(["-o " + "\"" +  final_location_and_name + "\"", "-sDEVICE=pdfwrite", "\"" + os.path.join(temporary_folder,TOC_FILE) +  "\"", "-c \"/setrgbcolor{0 mul 3 1 roll 0 mul 3 1 roll 0 mul 3 1 roll 0 mul add add setgray} def\"", "-f " + output_name_ps])    
     output_command = " ".join([GS_BINARY_PATH, ps_output_options])
-    p = sub.Popen(output_command, stdout=sub.PIPE, stderr=sub.PIPE, cwd=BASE_PATH)
+
+    p = sub.Popen(output_command, stdout=sub.PIPE, stderr=sub.PIPE, cwd=temporary_folder)
     output, errors = p.communicate()
     #print output, errors
     if errors != "": print errors
     
-    do_cleanup()
+    # remove the ps file
+    os.remove(output_name_ps)
     
-def merge_and_create_bookmarks(sortable, output_name):
+def merge_and_create_bookmarks(sortable, output_name, temporary_folder):
     # create bookmarks
-    f = open(os.path.join(BASE_PATH,TOC_FILE), 'w')
+    f = open(os.path.join(temporary_folder,TOC_FILE), 'w')
     str_files= ""
     for index,item in enumerate(sortable):
         f.write('[/Page ' + str(index + 1) + ' /View [ /FitH ] /Title ( ' + item[1].upper() + ' Sh ' + item[2].upper() + ' ) /OUT pdfmark' + "\n")
-        str_files += " " + "\"" + os.path.join(BASE_PATH,item[0])  + "\"" 
+        str_files += " " + "\"" + os.path.join(temporary_folder,item[0])  + "\"" 
     f.close()    
     
     # merge all pdfs
@@ -108,47 +113,44 @@ def merge_and_create_bookmarks(sortable, output_name):
     ps_output_options = "-dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dPDFFitPage -r300x300 -g4950x3510"
     output_command = " ".join([GS_BINARY_PATH, ps_output_options, "-sOutputFile=" + "\"" + output_name + "\"", '-f ' + str_files])
 
-    p = sub.Popen(output_command, stdout=sub.PIPE, stderr=sub.PIPE, cwd=BASE_PATH)
+    p = sub.Popen(output_command, stdout=sub.PIPE, stderr=sub.PIPE, cwd=temporary_folder)
     output, errors = p.communicate()
     #print output, errors
-    if errors != "": print errors
+    if errors != "": print errors  
 
-def do_cleanup():
-    # TODO: this should just go through sortable and add files in cleanup
-    for root, dirs, files in os.walk(BASE_PATH, topdown=False):
-        for name in files:        
-            if os.path.basename(name) not in KEEP_FILES:
-              # print("Would remove: " + name)
-              os.remove(os.path.join(root, name))
-        for name in dirs:        
-            os.rmdir(os.path.join(root, name))
-
-def init_start():
+def init_start(temporary_folder, zip_file):
     # keep a list of what we have so we don't delete them
-    for f in glob.glob(os.path.join(BASE_PATH,"*.zip")):
+    for f in glob.glob(os.path.join(sys.argv[1],"*.zip")):
         KEEP_FILES.append(os.path.basename(f)) # the file at present
         KEEP_FILES.append(os.path.basename(f)[0:-4] + PDF_EXTENSION) # the future PDF
     
     # time the overall operation
     total_start = time.time()
     # extract and process each zip file
-    for file in glob.glob(os.path.join(BASE_PATH,"*.zip")): 
-        # show user what is being worked on
-        print('Found zip: ' + file)
-        # time the operation
-        start = time.time()
-        
-        # extract zip file
-        unzip(file, BASE_PATH)    
-        # carry out merging, resizing, scaling, colour operations
-        real_start(file)
+    
+    # time the operation
+    start = time.time()
+    
+    # extract zip file
+    unzip(zip_file, temporary_folder)    
+    
+    # carry out merging, resizing, scaling, colour operations
+    real_start(temporary_folder, zip_file)
        
-        end = time.time()
-        print("Elapsed time: " + "%.2f" % (end-start))
+    end = time.time()
+    print("Elapsed time: " + "%.2f" % (end-start))
 
     total_end=time.time()
     print("TOTAL elapsed time: " + "%.2f" % (total_end-total_start))
-    
-        
+       
 if __name__ == "__main__":
-    init_start()
+    
+    # show name of file being processed
+    print('Found zip: ' + sys.argv[1])
+
+    # make folder with date time.
+    date_and_time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    # create temporary folder
+    tempfolder =  tempfile.mkdtemp(suffix=date_and_time, prefix='tmp', dir=None)
+    # now begin!
+    init_start(tempfolder, sys.argv[1])
